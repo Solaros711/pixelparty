e0c0128c750f4ffe2575a3bcb2db02552e2c91bc
Solaros711 <spurdy885@gmail.com>
2020-09-24 22:00:56 -0700

    Added Script that adds words from the words.json file

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose')
+const getWords = require('./word-script')
 
 // const port = 8000
 // const MESSAGES_PATH = './messages.txt'
 const port = process.env.PORT || 8000
+const path = 'words.json'
 
 const app = require('./app')()
 
@@ -15,12 +17,15 @@ const connectDatabase = async (hostname, databaseName) => {
         useCreateIndex: true
       }
     )
+    
   
     console.log(`database connected successfully at mongodb://${hostname}/${databaseName} ...`)
+    await getWords(path)
     return database
   }
 
 app.listen(port, async () => {
     await connectDatabase('localhost', 'pixelparty')
+    
     console.log(`server listening on port ${port}...`)
   })
