fa921b28085346e996c8ee71f60c0664f4d6b90a
barnetb <61876147+barnetb@users.noreply.github.com>
2020-10-06 16:07:25 -0700

    connected timer front/backend, fixed <select> # player style

--- client-react/src/game-components/Timer.js
+++ client-react/src/game-components/Timer.js
@@ -7,8 +7,10 @@ export default class Timer extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
+      // minutes: 1, 
+      // seconds: 15,
       timer: 30,
-      disabled: false,
+      // disabled: false,
       // socket: io('/timer')
     }
   }
@@ -25,18 +27,62 @@ export default class Timer extends React.Component {
     this.props.timerSocket.on('timer', timer => {
       this.setState({ timer })
     })
+
+
+    // this.myInterval = setInterval(() => {
+    //   const { seconds, minutes } = this.state
+    //   if (seconds > 0) {
+    //     this.setState(({ seconds }) => ({
+    //       seconds: seconds - 1
+    //     }))
+    //   }
+    //   if (seconds === 0) {
+    //     if (minutes === 0) {
+    //       clearInterval(this.myInterval)
+    //     } else {
+    //       this.setState(({ minutes }) => ({
+    //         minutes: minutes - 1,
+    //         seconds: 59
+    //       }))
+    //     }
+    //   }
+    // }, 1000)
+
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.myInterval)
   }
 
   handleTimesUp = () => this.props.onTimesUp(this.props.gameID)
 
   render () {
-    return (
-      <div>
-        <span id='timer'>00:{this.state.timer.toString().padStart(2, '0')}</span>
+    // const { minutes, seconds } = this.state
+    const { timer } = this.state
+    
+    return ( 
         <div>
-          <button onClick={this.handleTimesUp} style={{backgroundColor:"firebrick"}}>Test: End Round</button>
+            {/* { minutes === 0 && seconds === 0
+            ? <span style={{fontSize: "30px", color:"rgb(179, 67, 2)", textShadow:"2px 2px black"}}>TIME'S UP!</span>
+            :
+            minutes === 0 && seconds < 11
+            ?<span id="timer" style={{color:"rgb(179, 67, 2)", textShadow:"2px 2px black"}}>{minutes}:{seconds < 10 ? `0${seconds}` : seconds}</span>
+            :<span id="timer" style={{textShadow:"2px 2px black"}}>{minutes}:{seconds < 10 ? `0${seconds}` : seconds}</span>
+            } */}
+
+            { timer === 0
+            ? <span style={{fontSize: "30px", color:"rgb(179, 67, 2)", textShadow:"2px 2px black"}}>TIME'S UP!</span>
+            :
+            timer < 11
+            ?<span id="timer" style={{color:"rgb(179, 67, 2)", textShadow:"2px 2px black"}}>00:{this.state.timer.toString().padStart(2, '0')}</span>
+            :<span id="timer" style={{textShadow:"2px 2px black"}}>00:{this.state.timer.toString().padStart(2, '0')}</span>
+            }
+
+            {/* <span id='timer'>00:{this.state.timer.toString().padStart(2, '0')}</span> */}
+            <div>
+                <button onClick={this.handleTimesUp} style={{backgroundColor:"firebrick"}}>Test: End Round</button>
+            </div>
         </div>
-      </div>
     )
   }
 }
