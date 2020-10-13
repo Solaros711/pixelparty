cab187fbf7e2c97320be016565fcc151e0bd7265
Pete Jones <pwj2012@gmail.com>
2020-10-13 07:10:34 -0700

    fixed this.props > props

--- client-react/src/game-components/Timer.js
+++ client-react/src/game-components/Timer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Timer (props) {
   return (
     <div>
-      <span id='timer'>00:{this.props.timer.toString().padStart(2, '0')}</span>
+      <span id='timer'>00:{props.timer.toString().padStart(2, '0')}</span>
     </div>
   )
 }
